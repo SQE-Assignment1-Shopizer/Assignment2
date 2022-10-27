713edee529abcdc890fa7da37a7fd4bc73c3c2bf
FizzaSajjad <114175848+FizzaSajjad@users.noreply.github.com>
2022-10-27 02:09:02 -0700

    Update steps.js

--- steps.js
+++ steps.js
@@ -1,22 +1,23 @@
+//ClearAllFilterSteps
 const { I } = inject();
-// Add in your custom step files
 
-Given('I am on products page', () => {
-  // TODO: replace with your own step
-I.amOnPage('https://demo.reactstorefront.io/s/1');
-I.click("//body/div[@id='__next']/main[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/button[1]/div[1]/*[1]");
-I.click("//span[contains(text(),'LG')]");
-I.click("//body/div[@id='__next']/main[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/label[1]/span[1]/span[1]/input[1]");
-
+Given('I am on {string} category page',(product)  => {
+ 
+I.amOnPage(product);
 
 });
 
-When('I click on clear all button', () => {
-  // TODO: replace with your own step
+Given('I select {string} filter',(color)  => {
+  
+I.click(color);
+});
+
+When('I click clear all button', () => {
+  
   I.click("//body/div[@id='__next']/main[1]/div[1]/div[2]/div[1]/div[1]");
 });
 
-Then('I dont see clear all button', () => {
-  // TODO: replace with your own step
+Then('I dont see clear all button on the filters', () => {
+  
   I.dontSee("//button[contains(text(),'clear all')]");
 });
